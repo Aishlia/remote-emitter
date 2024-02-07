9e74fcf4e21c99ccb9e60d22ba8334831044300a
Aishlia <nai.julia@gmail.com>
2024-02-07 15:06:26 -0800

    @mentions link to user page

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,12 @@ export function extractCity(address) {
     return parts.length > 1 ? parts[1].trim() : '';
   }
   
+export function parseMessage(text) {
+    const userRegex = /@(\w+)/g;
+    const hashTagRegex = /#(\w+)/g;
+  
+    return text
+      .replace(userRegex, '<a href="/$1">@$1</a>')
+      .replace(hashTagRegex, '<a href="/tag/$1">#$1</a>');
+  }
+  
